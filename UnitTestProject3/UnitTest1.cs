e4604283aa9aa65de67bcd974c19616c06a9d4c8
Олександр Шкіндер <shkinder.o@gmail.com>
2018-12-08 10:57:16 +0200

    Добавьте файлы проекта.

---
+++ UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace OleksandrShkinder
+{
+    public class Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            System.Console.WriteLine("Test OK");
+        }
+    }
+}
