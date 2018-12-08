f6acf118853dd80c74e214ad4e89ca513c7857bb
Олександр Шкіндер <shkinder.o@gmail.com>
2018-12-08 13:25:48 +0200

    Add TestMethod2, change TestMethod1

--- UnitTestProject3/UnitTest1.cs
+++ UnitTestProject3/UnitTest1.cs
@@ -7,7 +7,15 @@ namespace OleksandrShkinder
         [Test]
         public void TestMethod1()
         {
-            System.Console.WriteLine("Test");
+            System.Console.WriteLine("Test 1");
         }
+
+        [Test]
+        public void TestMethod2()
+        {
+            System.Console.WriteLine("Test 2");
+        }
+
     }
+
 }
